4672dbaaffb2257b202ee17df0a21184ca7f17ef
Preet Patel <preet.patel2@ontariotechu.net>
2023-02-22 14:18:37 -0500

    Refactor rss_consumer to extract methods

--- rss_consumer.py
+++ rss_consumer.py
@@ -12,16 +12,15 @@ from confluent_kafka.schema_registry import SchemaRegistryClient
 # payload = RSSPayload.tyRSSPayload()
 # payload.damage_type=
 
-from rss_consumer_firebase import download_blobmain
+from rss_consumer_firebase import download_blob
+from rss_consumer_yolov5 import model_inference
 
-#................
-import os
 import sys
 from pathlib import Path
 
 import torch
 import torch.backends.cudnn as cudnn
-#...........
+
 ROOT = '/Road/yolov5/'
 if str(ROOT) not in sys.path:
         sys.path.append(str(ROOT))  # add ROOT to PATH
@@ -33,6 +32,7 @@ from yolov5.utils.general import (LOGGER, check_file, check_img_size, check_imsh
              increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from yolov5.utils.plots import Annotator, colors, save_one_box
 from yolov5.utils.torch_utils import select_device, time_sync
+
 # def configure():
 load_dotenv(find_dotenv())
 
@@ -100,59 +100,11 @@ def main(args):
             #                   rssClient.id,
             #                   rssClient.email))
 
-            #..................................................................................................
-
-    import numpy as np
-    from yolov5.utils.augmentations import letterbox
-    imagePath="us14--38-_jpg.rf.7067e4519392d181489b82ca5f8586c4.jpg"
-    img0 = cv2.imread(imagePath)
-
-    # source='pavement-cracks-1/test/images'
-    # dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
-    # im0s = im0s in dataset
-
-    # Padded resize
-    img = letterbox(img0, imgsz, stride, auto=pt)[0]
-
-    # Convert
-    img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
-    img = np.ascontiguousarray(img)
-
-    bs = 1  # batch_size
-
-    # Run inference
-    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
-    seen, windows, dt = 0, [], [0.0, 0.0, 0.0]
-    t1 = time_sync()
-    im = torch.from_numpy(img).to(device).cuda(device)
-    im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-    im /= 255  # 0 - 255 to 0.0 - 1.0
-    if len(im.shape) == 3:
-        im = im[None]  # expand for batch dim
-    t2 = time_sync()
-    dt[0] += t2 - t1
-
-    # Inference
-    #visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-    pred = model(im, augment=None, visualize=False)
-    t3 = time_sync()
-    dt[1] += t3 - t2
-
-    # NMS
-    pred = non_max_suppression(pred, conf_thres, iou_thres, None, False, max_det=1000)
-    dt[2] += time_sync() - t3
-
-    # Second-stage classifier (optional)
-    # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
-    seen += 1
-    det=pred[0]
-    if len(det):
-        # Rescale boxes from img_size to im0 size
-        pred2=scale_boxes(im.shape[2:], det[:, :4], img0.shape).round()
-        print(det.shape)
-        print('box: '+str(np.array(pred2.cpu())))
-        print('class: '+str(det[:,-1].cpu()))
-        print('confidence: '+str(np.array(det[:,-2].cpu())))
+    #..................................................................................................
+    
+    model_inference(imagePath="us14--38-_jpg.rf.7067e4519392d181489b82ca5f8586c4.jpg", model=model, imgsz=imgsz, stride=stride, 
+    pt=pt, device=device, conf_thres=conf_thres, iou_thres=iou_thres)
+    
         #break;
     #..................................................................................................
 
