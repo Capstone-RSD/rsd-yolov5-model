d58fc8dd721fc7c750e038395479ec3c78b2a117
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-02-28 18:00:49 -0500

    Imports for test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,42 +1,6 @@
 import unittest
-
-class TestClient(unittest.TestCase):
-
-    def test_client_initialization(self):
-        # initialize the client
-        client = google.protobuf.json_format.Parse(msg.value(),rssClient,ignore_unknown_fields=True)
-        
-        # check if the client is not None
-        self.assertIsNotNone(client)
-
-        # check if the client has the required attributes
-        self.assertTrue(hasattr(client, "blobs"))
-        self.assertTrue(len(client.blobs) > 0)
-
-        # check if the client blob has the required attributes
-        blob = client.blobs[0]
-        self.assertTrue(hasattr(blob, "blob_url"))
-
-    def test_model_inference(self):
-        # set the required parameters
-        model = model
-        imgsz = 224
-        stride = 32
-        pt = "cpu"
-        device = "cpu"
-        conf_thres = 0.5
-        iou_thres = 0.5
-
-        # download the blob and run the model inference
-        image_blob = client.blobs[0]
-        if image_blob.image == "image":
-            img = download_blob(image_blob.blob_url)
-        else:
-            self.fail("Video blob type expected")
-
-        if img is not None:
-            model_inference(imagePath=download_blob(image_blob.blob_url), model=model, imgsz=imgsz, stride=stride,
-            pt=pt, device=device, conf_thres=conf_thres, iou_thres=iou_thres)
-
-if __name__ == '__main__':
-    unittest.main()
+import argparse
+from unittest.mock import patch
+from parameterized import parameterized
+from io import StringIO
+from rss_consumer import main
