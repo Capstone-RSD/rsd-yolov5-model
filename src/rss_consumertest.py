ef8f5fe3ae9086097c42fc4de425b43df8230515
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-03-05 03:30:43 -0500

    Update rss_consumertest.py

--- src/rss_consumertest.py
+++ src/rss_consumertest.py
@@ -18,7 +18,7 @@ from confluent_kafka import Consumer
 # from confluent_kafka.serialization import SerializationContext, MessageField
 
 from google.protobuf import json_format
-from generated.rss_client_pb2 import Client
+# from generated.rss_client_pb2 import Client
 from yolov5.models.common import DetectMultiBackend
 from yolov5.utils.general import check_img_size
 from yolov5.utils.torch_utils import select_device
