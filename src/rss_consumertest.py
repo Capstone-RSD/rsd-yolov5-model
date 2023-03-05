1163185eb3565414b5ef0a5cff7654469908c4b2
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-03-05 05:08:12 -0500

    Update rss_consumertest.py

--- src/rss_consumertest.py
+++ src/rss_consumertest.py
@@ -16,10 +16,10 @@ from yolov5.models.common import DetectMultiBackend
 from yolov5.utils.general import check_img_size
 from yolov5.utils.torch_utils import select_device
 
+from rss_client_pb2 import Client
+
 from rss_consumer_firebase import download_blob
 from rss_consumer_yolov5 import model_inference
-
-from rss_client_pb2 import Client
 
 # Set the logging level to INFO
 logging.basicConfig(level=logging.INFO)
