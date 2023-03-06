c8b3660161091264095bed3dbf779059ff18692f
tiwaojo <tiwaojo@gmail.com>
2023-03-06 00:15:19 -0500

    fix(wip): fixing impl with kafka

--- src/rss_consumer_neo4j.py
+++ src/rss_consumer_neo4j.py
@@ -8,20 +8,22 @@ import folium
 class JsonToNeo4j:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
-        
+
     def close(self):
         self.driver.close()
-    
+
     # Method creates nodes in database
     def create_nodes(self, json_data):
         with self.driver.session() as session:
             query = "CREATE (r:Report { data: $data })"
             session.run(query, data=json.dumps(json_data))
-     
-    # Query to retrieve nodes that have lat and long properties. Returns list of dictionaries.            
+        map = folium.Map(location=[43.945082, -78.896740], zoom_start=13)
+        self.create_markers(map=map)
+
+    # Query to retrieve nodes that have lat and long properties. Returns list of dictionaries.
     def get_nodes_with_location(self):
       with self.driver.session() as session:
-        query = """ 
+        query = """
           MATCH (n:Node)
           WHERE EXISTS(n.latitude) AND EXISTS(n.longitude)
           RETURN n
@@ -32,17 +34,18 @@ class JsonToNeo4j:
         node = record["n"]
         nodes.append(node)
       return nodes
-    
+
     def create_markers(self, map):
       nodes = self.get_nodes_with_location()
+      print("Nodes: ",nodes)
       for node in nodes:
         try:
           lat = float(node["latitude"])
           lon = float(node["longitude"])
-          map = folium.Map(location=[43.9569614, -78.9014442], zoom_start=13)
           marker = folium.Marker(location=[lat, lon])
+          print("Longitude: %s",lon)
           marker.add_to(map)
-          map.save("neo_map.html")
         except (ValueError, TypeError):
           print("Value/type error occured during map creation")
           break
+      map.save("neo_map.html")
